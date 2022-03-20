7d6235e5fd8b72df99524ed5aad760fe39dd305c
danielbekhter <danielbekhter@gmail.com>
2022-03-20 01:16:10 +0100

    Extended SmartObjects

--- Assets/SmartObjects/SmartObject.cs
+++ Assets/SmartObjects/SmartObject.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public abstract class SmartObject : ScriptableObject
+[CreateAssetMenu(fileName = "Generic", menuName = "Smart Objects/Generic")]
+public class SmartObject : ScriptableObject
 {
+    public string objectName;
+    public GameObject physicalManifestation;
+    public enum Placing { //TODO What is with vitual objects, etc? Maybe have one menu and just grey out objects that can not be placed upon certain classification at all.
+        Wall, Floor, Table
+    }
+    public Placing canBePlacedOn;
+    public Sprite objectIconUI; //TODO Set standard as a placeholder image
+
+    // public ISmartObjectInstantiatable instantiatorScript; // Script that gets appended to the smart object's physical
+                                                             // manifestation in the scene and sets up the affected and interactive areas at according positions.
+                                                             // It would be easiest though to be able to assign everything in the inspector... But do I provide
+                                                             // the positions there then or just parts of the prefab or what?
 
     public GameObject interactiveArea = null;
-
     public GameObject affectedArea = null;
+
+    /*
+    public float interactiveAreaWidth = -1f;
+    public float interactiveAreaLength = -1f;
+    public float affectedAreaWidth = -1f;
+    public float affectedAreaLength = -1f;
+    */
 
     private bool inUse = false;
 
-    protected string affordance;
+    public string affordance;
 
     // False means the object is NOT in use
     public bool CheckAffordance(string flag)
