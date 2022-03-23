55dbe103d44d0f39aafa2831e6c26d2bc98d4ce9
danielbekhter <danielbekhter@gmail.com>
2022-03-23 20:36:13 +0100

    Fixed the agent communication task

--- Assets/Agents/Scripts/TaskSystem/AdvancedAgentTasks/AgentCommunicationTask.cs
+++ Assets/Agents/Scripts/TaskSystem/AdvancedAgentTasks/AgentCommunicationTask.cs
@@ -4,6 +4,8 @@ using System.Collections;
 // Tasks
 using System.Collections.Generic;
 using VirtualAgentsFramework.AgentTasks;
+// Action
+using System;
 // TextMeshPro
 using TMPro;
 
@@ -14,11 +16,13 @@ namespace VirtualAgentsFramework
         /// <summary>
         /// Define tasks for communication
         /// </summary>
-        public class AgentCommunicationTask : AgentLazyTask
+        public class AgentCommunicationTask : IAgentTask
         {
             private Agent agent = null;
             private GameObject tableau = null;
             private string message = "";
+
+            public event Action OnTaskFinished;
 
             // Constructor in case of a textual tableau as the means of communication
             public AgentCommunicationTask(GameObject tableau, string message)
@@ -27,17 +31,34 @@ namespace VirtualAgentsFramework
                 this.message = message;
             }
 
-            public override void Execute(Agent agent)
+            public void Execute(Agent agent)
             {
                 this.agent = agent;
-                tableau.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = message;
+                // Image child + TMP child
+                GameObject background = tableau.transform.GetChild(0).gameObject;
+                RectTransform backgroundRT = background.GetComponent<RectTransform>();
+                TextMeshProUGUI TMP_Text = tableau.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+                TMP_Text.text = message;
+                Debug.Log("TMP value: " + TMP_Text.GetPreferredValues().ToString());
+                backgroundRT.sizeDelta = new Vector2(TMP_Text.GetPreferredValues().x + 10, backgroundRT.sizeDelta.y);
                 Debug.Log("Communicated: " + message);
                 if (message == "")
-                    tableau.SetActive(false);
+                    background.SetActive(false);
                 else
-                    tableau.SetActive(true);
+                    background.SetActive(true);
+
                 // Trigger the TaskFinished event
-                FinishTask();
+                agent.StartCoroutine(FinishTaskCoroutine(0.1f));
+            }
+
+            public void Update() { }
+
+            private IEnumerator FinishTaskCoroutine(float waitingTime)
+            {
+                yield return new WaitForSeconds(waitingTime);
+                // Trigger the TaskFinished event
+                OnTaskFinished();
             }
         }
     }
