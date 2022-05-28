dbb7b32a6d4eb887ff44b2dc022f0a7703186600
danielbekhter <danielbekhter@gmail.com>
2022-05-28 10:32:03 +0200

    Renamed smart objects and interactions to Spanish

--- Assets/Interactions/PointingInteraction.cs
+++ Assets/Interactions/PointingInteraction.cs
@@ -7,15 +7,23 @@ using VirtualAgentsFramework;
 public class PointingInteraction : Interaction
 {
     public string walkingSentenceSource = "I am walking towards ***.";
-    public string walkingSentenceTarget = "Kõnnin ***a/i poole.";
+    public string walkingSentenceTarget = "Camino hacia ***.";
+    //public string walkingSentenceTarget = "Kõnnin ***a/i poole.";
+
+    public string rotatingSentenceSource = "I am turning towards ***.";
+    public string rotatingSentenceTarget = "Me dirijo hacia ***.";
+
+    public string pointingSentenceSource = "I am pointing at ***.";
+    public string pointingSentenceTarget = "Señalo ***.";
 
     void OnEnable ()
     {
         base.flag = "Pointing";
 
         // "***" are placeholders for the smart object's name
-        base.sentenceSource = "This is ***."; 
-        base.sentenceTarget = "See on ***.";
+        base.sentenceSource = "This is ***.";
+        base.sentenceTarget = "Este/Esta es ***";
+        //base.sentenceTarget = "See on ***.";
     }
 
     public override void Perform (Agent agent, SmartObjectInstance pointableSmartObjectInstance)
@@ -25,7 +33,8 @@ public class PointingInteraction : Interaction
         string temp2 = walkingSentenceTarget.Replace("***", pointableSmartObjectInstance.smartObject.nameTarget);
         string sentence = temp2 + "\n" + "(" + temp1 + ")";
         agent.Communicate(sentence);
-
+        // Additional wait
+        agent.WaitForSeconds(1);
         //TODO Implement these as subinteractions or something or at least check the list of required affordances and abilities
         // Walk to the marker
         agent.WalkTo(pointableSmartObjectInstance.affectedAreaGameObject);
@@ -34,15 +43,35 @@ public class PointingInteraction : Interaction
         //agent.RotateTowards(camera.transform.position);
         // Rotate to be right from interactive area
         //agent.RotateRelative(pointableSmartObjectInstance.interactiveAreaGameObject.transform.position, true);
+        temp1 = rotatingSentenceSource.Replace("***", pointableSmartObjectInstance.smartObject.nameSource);
+        temp2 = rotatingSentenceTarget.Replace("***", pointableSmartObjectInstance.smartObject.nameTarget);
+        sentence = temp2 + "\n" + "(" + temp1 + ")";
+        agent.Communicate(sentence);
+
+        agent.WaitForSeconds(1);
+
         agent.RotateTowards(pointableSmartObjectInstance.interactiveAreaGameObject.transform.position);
-        // Point
+
         temp1 = sentenceSource.Replace("***", pointableSmartObjectInstance.smartObject.nameSource);
         temp2 = sentenceTarget.Replace("***", pointableSmartObjectInstance.smartObject.nameTarget);
         sentence = temp2 + "\n" + "(" + temp1 + ")";
         agent.Communicate(sentence);
+
+        agent.WaitForSeconds(5);
+
+        // Point
+        temp1 = pointingSentenceSource.Replace("***", pointableSmartObjectInstance.smartObject.nameSource);
+        temp2 = pointingSentenceTarget.Replace("***", pointableSmartObjectInstance.smartObject.nameTarget);
+        sentence = temp2 + "\n" + "(" + temp1 + ")";
+        agent.Communicate(sentence);
+
+        agent.WaitForSeconds(1);
+
         Debug.Log("Current interactive area: " + pointableSmartObjectInstance.interactiveArea);
         agent.PointTo(pointableSmartObjectInstance.interactiveAreaGameObject);
         //agent.PlayAnimation("Pointing");
+
+        agent.WaitForSeconds(1);
 
         agent.Communicate("");
 
